32cf88306eb05020d276124e7463a3c4e9ca18a0
missvader <vanepalazon611@gmail.com>
2022-10-11 17:41:01 +0200

    Ex 3. moviesAverageOfDirector()

--- src/films.js
+++ src/films.js
@@ -6,22 +6,34 @@ function getAllDirectors(movies) {
   let result= [];
   result = movies.map(movie => movie.director);
   
-  //console.log("EXERCICE 1 ->", result);
+  console.log("EXERCICE 1 ->", result);
   return result;
 }
 
 
 // Exercise 2: Get the films of a certain director
+  //usamos filter para crear nuevo array con las movies que cumplan condicion
 function getMoviesFromDirector(movies, director) {
- 
  let moviesFromDirector = movies.filter(movie => movie.director === director);
  console.log("Exercice 2 ->", moviesFromDirector);
  return moviesFromDirector;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
-function moviesAverageOfDirector(array, director) {
-  
+  //iteramos sobre array con forEach, si se cumple condic. guardamos scores en nueva variable.
+  //con reduce() sumamos las scores del array
+function moviesAverageOfDirector(movies, director) {
+  let averageOfDirector = 0;
+  let scoreDirector = [];
+  movies.forEach(movie => {
+    if (movie.director === director){
+      scoreDirector.push(movie.score)
+    }
+  });
+  let sumaScore = scoreDirector.reduce((a,b)=> a+b);
+  averageOfDirector = sumaScore / scoreDirector.length;
+  console.log("Exercice 3 ->",averageOfDirector);
+  return averageOfDirector;
 }
 
 // Exercise 4:  Alphabetic order by title 
